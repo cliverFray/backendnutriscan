19579e3e0846bc27dd0d2e0e0751d02dec33c09b
Cliver <u20201b288@upc.edu.pe>
2024-11-14 01:25:44 -0500

    Feat: update user data

---
+++ nutriscan/serializers/userSerializers/UserProfileEditSerializer.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from ...models import AditionalInfoUser
+
+class UserDataSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+
+class AditionalInfoUserDataSerializer(serializers.ModelSerializer):
+    user = UserDataSerializer(required=False)
+
+    class Meta:
+        model = AditionalInfoUser
+        fields = ['user', 'userDNI', 'userPhone', 'userPlace']
