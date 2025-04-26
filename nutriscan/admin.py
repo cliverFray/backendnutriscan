1ed50b2bf6a30b41425fcf693a57e16c106e1e14
Cliver <u20201b288@upc.edu.pe>
2025-04-26 01:14:22 -0500

    Feat: se personalizó el panel para manejar la informacion adicional

--- nutriscan/admin.py
+++ nutriscan/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.contrib.auth.models import User
+from .models import AditionalInfoUser
 
-# Register your models here.
+# Crear el Inline
+class AditionalInfoUserInline(admin.StackedInline):
+    model = AditionalInfoUser
+    can_delete = False
+    verbose_name_plural = 'Información adicional'
+    fk_name = 'user'
+
+# Extender el admin original de User
+class CustomUserAdmin(DefaultUserAdmin):
+    inlines = (AditionalInfoUserInline,)
+
+# Re-registrar el modelo User con nuestro CustomAdmin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
