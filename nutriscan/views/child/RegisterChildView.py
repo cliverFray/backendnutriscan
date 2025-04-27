b2d9f75f9e449fbe26b0e47aa846ddef6b0a0490
Cliver <u20201b288@upc.edu.pe>
2025-04-27 17:19:16 -0500

    Feat: Se actualizó las vista registrar y actualizar niño

--- nutriscan/views/child/RegisterChildView.py
+++ nutriscan/views/child/RegisterChildView.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from ...models import Child
+from ...models import Child,GrowthHistory
 from ...serializers.childSerializers.childSerializer import ChildSerializer
 from rest_framework.permissions import IsAuthenticated
 
@@ -14,6 +14,15 @@ class RegisterChildView(APIView):
 
         serializer = ChildSerializer(data=child_data)
         if serializer.is_valid():
-            serializer.save(user=request.user)  # Pasa el usuario autenticado como argumento
+            #serializer.save(user=request.user)  # Pasa el usuario autenticado como argumento
+            child = serializer.save(user=request.user)  # Guarda el niño
+
+            # Crear el primer registro de GrowthHistory si tiene peso y talla
+            if child.childCurrentWeight is not None and child.childCurrentHeight is not None:
+                GrowthHistory.objects.create(
+                    child=child,
+                    weight=child.childCurrentWeight,
+                    height=child.childCurrentHeight
+                )
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
