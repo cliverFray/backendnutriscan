28fb23ab115ee8440752f1f21c5ec27ab0cca92a
Cliver <u20201b288@upc.edu.pe>
2025-07-04 01:36:53 -0500

    Feat: Se agregó el campo tipo al modelo nutritionalTIps

--- nutriscan/serializers/nutritionTip/NutritionTipSerializer.py
+++ nutriscan/serializers/nutritionTip/NutritionTipSerializer.py
@@ -4,4 +4,4 @@ from ...models import NutritionTip
 class NutritionTipSerializer(serializers.ModelSerializer):
     class Meta:
         model = NutritionTip
-        fields = ['id', 'title', 'description', 'calories', 'portion_size', 'image_url', 'date_created']
+        fields = ['id', 'title', 'description', 'calories', 'portion_size', 'image_url', 'date_created','tipo']
